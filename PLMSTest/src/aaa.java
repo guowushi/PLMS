7be2e7e6cae3aea49b92e5533791fcba4d099e04
guowushi <guowushi@hotmail.com>
2012-04-08 00:19:09 +0800

    Failed commit: Default

---
+++ PLMSTest/src/aaa.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: guowushi
+ * Date: 12-4-8
+ * Time: 上午12:18
+ * To change this template use File | Settings | File Templates.
+ */
+public class aaa {
+}
