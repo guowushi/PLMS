fad8b710d2bf42d1e8abb32814851eedcfd092fd
guowushi <guowushi@hotmail.com>
2012-04-08 22:11:08 +0800

    new

---
+++ PLMSCommon/src/PLMS/frames/IDownResponseMessage.java
@@ -0,0 +1,11 @@
+package PLMS.frames;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: guowushi
+ * Date: 12-4-8
+ * Time: 上午11:01
+ * To change this template use File | Settings | File Templates.
+ */
+public interface IDownResponseMessage extends IDownMessage, IResponseMessage {
+}
